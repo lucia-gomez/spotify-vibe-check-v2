12f64fad49349d59efb02a88905b382f8d07d852
lucia-gomez <isabel.lucia.gomez@gmail.com>
2022-06-06 21:41:53 -0700

    update redirect URI

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV !== 'production';
-export const redirectUri = isDev ? "http://localhost:3000/callback/" : "https://spotify-vibe-check.herokuapp.com/callback/";
+export const redirectUri = isDev ? "http://localhost:3000/" : "https://spotify-vibe-check.herokuapp.com/";
 export const clientId = "38632b26273a47e28539a2b34468d660";
 
 export const scopes = [
